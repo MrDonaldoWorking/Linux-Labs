3976b00ca976d0073e6b2567b003d47c4f21375a
MrDonaldoPublic <mr.donaldo.cf@gmail.com>
2020-11-06 21:38:37 +0300

    Partially done

--- 3/script.sh
+++ 3/script.sh
@@ -6,17 +6,17 @@ echo "doing 1..."
 
 for user in $(compgen -u); do
     echo user $user has id $(id -u $user)
-done > works3.log
+done > work3.log
 
 #2
 echo "doing 2..."
 
-chage -l root | head -n1 | cut -d : -f 2 >> works3.log
+chage -l root | head -n1 | cut -d : -f 2 >> work3.log
 
 #3
 echo "doing 3..."
 
-awk -F : 'ORS="," {print $1}' /etc/group >> works3.log
+awk -F : 'ORS="," {print $1}' /etc/group >> work3.log
 
 #4
 echo "doing 4..."
@@ -26,34 +26,54 @@ echo Be careful > /etc/skel/readme.txt
 #5
 echo "doing 5..."
 
-useradd u1 $(openssl passwd 12345678)
+useradd u1 -p $(openssl passwd 12345678)
 
 #6
 echo "doing 6..."
 
+groupadd g1
+
 #7
 echo "doing 7..."
+
+usermod -G g1 u1
 
 #8
 echo "doing 8..."
 
+id u1 | cut -d ' ' -f 1,3 >> work3.log
+
 #9
 echo "doing 9..."
+
+usermod -G g1 user
 
 #10
 echo "doing 10..."
 
+grep "^g1:" /etc/group | cut -d : -f 4 >> work3.log
+
 #11
 echo "doing 11..."
+
+usermod --shell /usr/bin/mc u1
 
 #12
 echo "doing 12..."
 
+useradd u2 -p $(openssl passwd 87654321)
+
 #13
 echo "doing 13..."
 
+mkdir /home/test13
+cp work3.log /home/test13/work3-1.log
+cp work3.log /home/test13/work3-2.log
+
 #14
 echo "doing 14..."
+
+
 
 #15
 echo "doing 15..."
